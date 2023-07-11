efa6760d7db1c868c7ae28440fc19db21af87877
moemode <pfeffer.moritz@gmail.com>
2023-07-11 22:11:01 +0200

    Start on ch. 4 'ownership' of rust book

---
+++ ownership/src/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    // Each value in Rust has an owner.
+    // There can only be one owner at a time.
+    // When the owner goes out of scope, the value will be dropped.
+    let mut s = String::from("hello");
+    /* There is a natural point at which we can return the memory our String 
+    needs to the allocator: when s goes out of scope. When a variable goes out of scope, 
+    Rust calls a special function for us. This function is called drop, and it’s where the author of String can put the 
+    code to return the memory. Rust calls drop automatically at the closing curly bracket.
+     */
+    {
+        let q = String::from("hello");
+        // upon leaving this scope the value owned by q is droped
+    }
+    s.push_str(" world");
+    println!("{s}");
+    takes_ownership(s);
+
+}
+
+fn moveit() {
+    let x = 5;
+    let y = x; // simple fixed size values live on the stack and are copied
+    let s1 = String::from("hello");
+    // "invalidate s1"
+    // Rust will never automatically create “deep” copies of your data. 
+    // Therefore, any automatic copying can be assumed to be inexpensive in terms of runtime performance.
+    let mut s2 = s1;
+    s2 = takes_and_gives_back(s2);
+    //would:fail println!("{} world!", s1);
+}
+
+fn takes_ownership(some_string: String) { // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) { // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
+
+// taking ownership and then returning ownership with every function is a bit tedious.
+// better use references.
+// This function takes a String and returns one
+fn takes_and_gives_back(a_string: String) -> String {
+a_string  // a_string is returned and moves out to the calling function
+}
