f7e841403f8d4c65c0bbe6395fb0c84d64081994
moemode <pfeffer.moritz@gmail.com>
2023-07-15 11:13:48 +0200

    Go through modules 7.2

---
+++ pkgs-modules/src/main.rs
@@ -0,0 +1,7 @@
+use crate::garden::vegetable::Asparagus;
+
+pub mod garden;
+fn main() {
+    let plant = Asparagus {};
+    println!("I'm growing {:?}!", plant);
+}
---
+++ restaurant/src/lib.rs
@@ -0,0 +1,12 @@
+mod front_of_house {
+    mod hosting {
+        fn add_to_wait() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve() {}
+        fn take_payment() {}
+    }
+}
