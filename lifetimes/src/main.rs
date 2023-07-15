1a8a5bc8536064817eb810b0b65deef652b84425
Moritz Pfeffer <st152880@stud.uni-stuttgart.de>
2023-07-15 21:08:19 +0200

    Work through 10.3 ref lifetimes

---
+++ lifetimes/src/main.rs
@@ -0,0 +1,35 @@
+use std::fmt::Display;
+
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first };
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
