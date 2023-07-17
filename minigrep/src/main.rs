d255af5bd3f6a0e38321b8de8b53b2c1cabe07f4
moemode <pfeffer.moritz@gmail.com>
2023-07-17 12:10:58 +0200

    Work through 12.4 implement search

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,8 +8,8 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
     if let Err(e) = minigrep::run(config){
         println!("Application error: {e}");
         process::exit(1);
